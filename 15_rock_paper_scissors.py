deb3984b53a0b9c388b3db0e8d668bd045976e29
Teboho Thapeli <tm.thapeli@gmail.com>
2022-09-20 19:20:45 +0200

    completed rock, paper, scissors and face detection

--- 15_rock_paper_scissors.py
+++ 15_rock_paper_scissors.py
@@ -1,5 +1,83 @@
+from random import choice
+
+play_options = {"r": "rock", "p": "paper", "s": "scissors"}
+l, w = "lost", "won"
+
+# from user's perspective:
+game_logic = {"r": {"p": l, "s": w}, "p": {"r": w, "s": l}, "s": {"r": l, "p": w}}
+
+
+def draw_line(rl_char: str = "|", cen_chars: str = " ", inner_chars: str = " ") -> None:
+    if cen_chars == "-":
+        inner_chars = cen_chars
+
+    print(f"{rl_char}{cen_chars.center(68, inner_chars)}{rl_char}")
+
+
+def print_scores(user_score: int, computer_score: int, game_num: int) -> None:
+    draw_line(rl_char="+", cen_chars="-")
+
+    message = ""
+
+    if user_score > computer_score:
+        message = "YOU WON"
+    elif computer_score > user_score:
+        message = "YOU LOST"
+    else:
+        message = "IT WAS A TIE"
+
+    draw_line(cen_chars=message)
+    draw_line()
+    draw_line(cen_chars=f"Computer: {computer_score}/{game_num}")
+    draw_line(cen_chars=f"You: {user_score}/{game_num}")
+    draw_line(rl_char="+", cen_chars="-")
+    print()
+
+
+def get_user_input() -> str:
+    while True:
+        user_input = input(
+            "\nDo you want to play r (rock), p (paper) or s (scissors)?: (q to quit)\n"
+        ).lower()[0]
+
+        if user_input in list(play_options) + ["q"]:
+            return user_input
+
+        print("Invalid input entered. Try again.")
+
+
 def main():
-    pass
+    user_score, computer_score, game_num = 0, 0, 0
+
+    while True:
+        user_input = get_user_input()
+
+        if user_input == "q":
+            if game_num:
+                print_scores(user_score, computer_score, game_num)
+
+            print("\nQuitting program...")
+            quit()
+
+        game_num += 1
+        computer_input = choice(list(play_options))
+
+        print(f"The computer chose: {play_options[computer_input]}")
+
+        if user_input == computer_input:
+            computer_score += 1
+            user_score += 1
+
+            print("Tied for this round")
+        else:
+            result = game_logic[user_input][computer_input]
+
+            if result == "won":
+                user_score += 1
+            else:
+                computer_score += 1
+
+            print(f"You {result} this round.")
 
 
 if __name__ == "__main__":
--- 16_face_detection.py
+++ 16_face_detection.py
@@ -1,5 +1,23 @@
+import cv2
+
+
 def main():
-    pass
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+    img_file = "arsenal.jpg"
+    img = cv2.imread(img_file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow("img", img)
+    cv2.waitKey()
+
+    img_name, img_extension = img_file.split(".")
+    cv2.imwrite(f"{img_name}_faces_detected.{img_extension}", img)
 
 
 if __name__ == "__main__":
