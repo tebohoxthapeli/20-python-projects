2318561de1ac9a1720e61052bc1f884938a532fe
Teboho Thapeli <tm.thapeli@gmail.com>
2022-09-20 22:09:58 +0200

    completed automation and web scraper

--- 17_automation.py
+++ 17_automation.py
@@ -1,5 +1,30 @@
+from os import environ
+from requests import post
+from schedule import run_pending, every
+from time import sleep
+
+
+def send_text():
+    phone = environ.get("phone", "")
+
+    res = post(
+        "https://textbelt.com/text",
+        {
+            "phone": phone,
+            "message": "Hello world!",
+            "key": "textbelt",
+        },
+    ).json()
+
+    print(res)
+
+
 def main():
-    pass
+    every().day.at("20:28").do(send_text)
+
+    while True:
+        run_pending()
+        sleep(1)
 
 
 if __name__ == "__main__":
--- 18_web_scraper.py
+++ 18_web_scraper.py
@@ -1,5 +1,18 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+
 def main():
-    pass
+    res = get(
+        "https://medium.com/towards-data-science/the-5-basic-statistics-concepts-data-scientists-need-to-know-2c96740377ae"
+    )
+
+    soup = BeautifulSoup(res.content, "html.parser")
+    h1s = soup.find_all(name="h1", recursive=True)
+    print()
+
+    for h1 in h1s:
+        print(h1.getText())
 
 
 if __name__ == "__main__":
